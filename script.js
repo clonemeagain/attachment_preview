f2e70f794b3f65318c1a731a5de8bd0b5d98b4a4
Aaron <clonemeagain@gmail.com>
2019-03-02 14:33:25 +1100

    Incorporates #29 and cleans up a bit

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-//'use strict';
-// Attachment Preview Script.
-
+"use strict";
+// Attachment Preview Script, with HTML Sanitizer.
 // Setup default config options
-var sanitizer = sanitizer || {},
-    pluginConfig = {
+var pluginConfig = {
         open_attachments: 'normal',
         text_hide: 'Hide Attachment',
         text_show: 'Show Attachment',
@@ -20,21 +18,20 @@ var sanitizer = sanitizer || {},
  * 
  * @returns void
  */
- function startPlugin(){
+$(document).ready(function(){ // I thought we were supposed to use $(document).on('ready',function(){...
 
-        // Don't we need  pjax:success any more?
-        var attachments = $(".ap_embedded:not(.hidden)");
-        if (attachments.length) {
-            console.log("Fetching " + attachments.length + " non-hidden attachment[s].");
-            attachments.trigger('ap:fetch');
-        }
-        if (pluginConfig.open_attachments === 'new-tab') {
-            $('a.file').prop('target', '_blank');
-        }
+    // Don't we need  pjax:success any more?
+    var attachments = $(".ap_embedded:not(.hidden)");
+    if (attachments.length) {
+        console.log("Fetching " + attachments.length + " non-hidden attachment[s].");
+        attachments.trigger('ap:fetch');
+    }
+    if (pluginConfig.open_attachments === 'new-tab') {
+        $('a.file').prop('target', '_blank');
+    }
 
-        console.log("AP: Plugin running, initial fetch limit configured to " + pluginConfig.limit + ".");
-}
-$(document).ready(startPlugin); // ugly.. 
+    console.log("AP: Plugin running, initial fetch limit configured to " + pluginConfig.limit + ".");
+});
 
 /**
  * Toggle function for buttons, shows the attachment's wrapper element and
@@ -61,13 +58,14 @@ function ap_toggle(item, key) {
 
 /**
  * Slightly more convoluted that the other types Load the PDF into an Object
- * Blob and shove it into the <embed> :-)
+ * Blob and shoves it into the <object> :-)
  * 
  * @param id id of the element to inject the pdf
  * @param url of the file to convert into a Blob and inject
  */
 function fetch_pdf(id, url) {
-    var pdf = document.getElementById(id), URL = window.URL || window.webkitURL;
+    var pdf = document.getElementById(id),
+        URL = window.URL || window.webkitURL;
     if ( /* @cc_on!@ */ false || !!document.documentMode) {
         // IE still cant display a PDF inside an <object>
         console.log("Why Microsoft?");
@@ -86,32 +84,71 @@ function fetch_pdf(id, url) {
         var blob = new Blob([ab], {
             type: "application/pdf"
         });
-        console.log("Loaded " + blob.type + " of size: " + blob.size);
+        //console.log("Loaded " + blob.type + " of size: " + blob.size);
         // Convert the binary blob of PDF data into an Object URL
         var object_url = URL.createObjectURL(blob);
         if (!object_url) {
             console.log("Failed to construct usable ObjectURL for the attachment. Bailing");
             return;
         }
-        console.log("Embedding PDF with ObjectURL: " + object_url);
-
         var newpdf = pdf.cloneNode();
-        newpdf.setAttribute('type','application/pdf');
+        newpdf.setAttribute('type', 'application/pdf');
         newpdf.setAttribute('data', object_url);
-        newpdf.setAttribute('style','display:inline-block');
-        //newpdf.onload = function(){
-// I think we are supposed to remove the ObjectURL's at some point as well.. 
-            //URL.revokeObjectURl(object_url);
-           // console.log("Would have revoked the URL here..");
-      //  };
+        newpdf.setAttribute('style', 'display:inline-block');
+        newpdf.onload = function(){
+        // I think we are supposed to remove the ObjectURL's at some point:
+        	URL.revokeObjectURL(object_url);
+        };
         // Replace the node with our new one which displays it:
-        console.log("Replacing old object..");
         pdf.parentNode.replaceChild(newpdf, pdf);
         // prevent repeated fetch events from re-fetching
-        newpdf.setAttribute('data-url','');
-        };
+        newpdf.setAttribute('data-url', '');
+    };
     req.send();
 }
+
+// The HTML inject methods will probably fail at some point, but for now.. I'll just leave this here.
+// src: https://gist.github.com/ufologist/5a0da51b2b9ef1b861c30254172ac3c9
+var sanitizer = {};
+(function($) {
+    var safe = '<a><b><blockquote><dd><div><dl><dt><em><h1><h2><h3><h4><i><img><li><ol><p><pre><s><sup><sub><strong><strike><ul><br><hr><table><th><tr><td><tbody><tfoot>';
+
+    function trimAttributes(node) {
+        $.each(node.attributes, function() {
+            var attrName = this.name;
+            var attrValue = this.value;
+            // we could filter the "bad" attributes, or just purge them all..
+            $(node).removeAttr(attrName);
+        });
+    }
+    sanitizer.sanitize = function(html) {
+        html = strip_tags(html, safe);
+        var output = $($.parseHTML('<div>' + $.trim(html) + '</div>', null,
+                false));
+        output.find('*').each(function() {
+            trimAttributes(this);
+        });
+        return output.html();
+    }
+
+    // http://locutus.io/php/strings/strip_tags/ filter the html to only those
+    // acceptable tags defined above as safe
+    function strip_tags(input, allowed) {
+        allowed = (((allowed || '') + '').toLowerCase().match(
+                /<[a-z][a-z0-9]*>/g) || []).join('')
+        var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi
+        var commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi
+        return input
+                .replace(commentsAndPhpTags, '')
+                .replace(
+                        tags,
+                        function($0, $1) {
+                            return allowed
+                                    .indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0
+                                    : ''
+                        })
+    }
+})(jQuery);
 
 /**
  * Setup handler to receive Attachments Preview Fetch events, and act on them.
@@ -156,5 +193,6 @@ $(document).on('ap:fetch', function(e) {
         elem.data('type', '');
     }
 });
+
 
 console.log("AP: Plugin loaded");
