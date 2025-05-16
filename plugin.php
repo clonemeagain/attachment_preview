fc2b04d9a362b552078f5c2cb5939ec2271029b2
awere <awere@tdj.com.au>
2025-05-16 17:01:13 +1000

    Bringing it up to the 1.18 world.. finally time to upgrade hey?

--- plugin.php
+++ plugin.php
@@ -1,11 +1,15 @@
 <?php
 
-return array(
-    'id' => 'clonemeagain:attachment_preview', # notrans
-    'version' => '1.11',
-    'name' => 'Attachment Inline Plugin',
-    'author' => 'clonemeagain@gmail.com',
-    'description' => 'Modifies the page to include as many attachments as would make sense, directly into the Thread.',
-    'url' => 'https://github.com/clonemeagain/attachment_preview',
-    'plugin' => 'class.AttachmentPreviewPlugin.php:AttachmentPreviewPlugin'
-);
+//error_reporting(E_ALL);
+//ini_set('display_errors', '1');
+//ini_set('display_startup_errors', '1');
+
+return [
+  'id'          => 'clonemeagain:attachment_preview', # notrans
+  'version'     => '1.2',
+  'name'        => 'Attachment Inline Plugin',
+  'author'      => 'clonemeagain@gmail.com',
+  'description' => 'Modifies the page to include as many attachments as would make sense, directly into the Thread.',
+  'url'         => 'https://github.com/clonemeagain/attachment_preview',
+  'plugin'      => 'class.AttachmentPreviewPlugin.php:AttachmentPreviewPlugin',
+];
